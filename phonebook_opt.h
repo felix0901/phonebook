453a96f059d9bcfa636fe5cb3fcf7bdf45c8ff59
nashi5566 <jennifer710846@gmail.com>
2018-03-01 01:14:50 +0800

    Create new structure

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,8 +5,8 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
+#define OPT 1
+typedef struct __PHONE_BOOK_ENTRY_OLD {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
@@ -17,6 +17,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry_old;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    entry_old *old;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
